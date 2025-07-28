b32a781955fab417480821b30157368c05a8e276
SILA <106426620+kongchansila05@users.noreply.github.com>
2025-07-28 22:48:49 +0700

    Update cors.go

--- middleware/cors.go
+++ middleware/cors.go
@@ -7,7 +7,7 @@ import (
 func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Allow local frontend
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		w.Header().Set("Access-Control-Allow-Origin", "https://frontend-wwb99-production.up.railway.app")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
